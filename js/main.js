256e09fa05462dbe5053b0d908b3753f18221408
bryanalonso1993 <bryalmeyda.7@gmail.com>
2019-08-08 19:26:27 -0500

    frontend 1er

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/** CÓDIGO JQUERY   */
+
+function executeButtons() {
+    if (confirm("Are yo sure?")) {
+        alert("Asd asd asd");
+    } else {
+        alert("assaaa");
+    }
+}
+
+
+/*
+window.onload = executeButtons;
+*/
